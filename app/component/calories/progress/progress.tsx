1f7c08f77080bf6be70cc726ca3eddde14b18da6
jwkaterina <jwkaterina@gmail.com>
2023-09-08 13:27:44 +0200

    add calories to progress bar

--- app/component/calories/progress/progress.tsx
+++ app/component/calories/progress/progress.tsx
@@ -2,13 +2,31 @@ import './progress-bar.css'
 import './progress-completed.css'
 import './progress-exceeded.css'
 import './progress-target.css'
+import { useContext } from 'react';
+import { CaloriesContext, PortionsCheckedContext } from '../../../context/food-context'
 
 export const ProgressBar = () => {
-    const Calories = 2000
+
+    const fruitCal = useContext(CaloriesContext).fruit * useContext(PortionsCheckedContext).portionsChecked.fruit;
+    console.log(useContext(PortionsCheckedContext).portionsChecked.fruit)
+    const veggiesCal = useContext(CaloriesContext).veggies * useContext(PortionsCheckedContext).portionsChecked.veggies;
+    const carbsCal = useContext(CaloriesContext).carbs * useContext(PortionsCheckedContext).portionsChecked.carbs;
+    const protsCal = useContext(CaloriesContext).prots * useContext(PortionsCheckedContext).portionsChecked.prots;
+    const fatsCal = useContext(CaloriesContext).fats * useContext(PortionsCheckedContext).portionsChecked.fats;
+    const sweetsCal = useContext(CaloriesContext).sweets * useContext(PortionsCheckedContext).portionsChecked.sweets;
+
+    const calculateCalories = () => {
+     
+        const calories = fruitCal + veggiesCal + carbsCal + protsCal + fatsCal + sweetsCal;
+        console.log(calories)
+
+        return calories;
+    }
+
     return  (
     <div className="ProgressBar">
         <div className="ProgressBar__completed"></div>
         <div className="ProgressBar__exceeded"></div>
-        <div className="ProgressBar__calories">{Calories} k</div>
+        <div className="ProgressBar__calories">{calculateCalories()} kcal</div>
     </div>)
 }
