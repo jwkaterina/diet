82aed6b440a690097e2ae74dc3ce971a215ee178
jwkaterina <jwkaterina@gmail.com>
2023-09-13 19:15:01 +0200

    handle btn clicks in progress settings section

--- app/settings/component/cells/progress-cell.tsx
+++ app/settings/component/cells/progress-cell.tsx
@@ -6,14 +6,16 @@ export const ProgressCell: FunctionComponent<ProgressCellProps> = (props) => {
     return (
         <>
             <h3 className="Settings__time-cell">{props.children}</h3>
-            <div className="Settings__number-minus-btn Settings__time-cell Settings__controls-cell"></div>
+            <div className="Settings__number-minus-btn Settings__time-cell Settings__controls-cell" onClick={props.onMinusClick}></div>
             <h3 className="Settings__time-cell Settings__count-cell">{props.count}</h3>
-            <div className="Settings__number-plus-btn Settings__time-cell Settings__controls-cell"></div>
+            <div className="Settings__number-plus-btn Settings__time-cell Settings__controls-cell" onClick={props.onPlusClick}></div>
         </> 
         )
 }
 
 export interface ProgressCellProps {
     children: React.ReactNode;
-    count: number
+    count: number,
+    onPlusClick: () => void
+    onMinusClick: () => void
 }
--- app/settings/component/section/progress-section.tsx
+++ app/settings/component/section/progress-section.tsx
@@ -1,17 +1,50 @@
 'use client'
 
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useContext } from 'react';
 import { SettingsSection } from './settings-section';
 import { ProgressCell } from '../cells/progress-cell';
+import { MealsContext } from '../../../context/meals-context';
 
 export const ProgressSection: FunctionComponent = (props) => {
+    const {meals, setMeals} = useContext(MealsContext);
+    const {firstMeal, lastMeal, mealsNumber} = meals;
+
+    const handeFirstMealMinusClick = () => {
+        if (firstMeal < 1) return;
+        setMeals({...meals, firstMeal: firstMeal - 1})
+    }
+
+    const handleFirstMealPlusClick = () => {
+        if (firstMeal >= 23 || lastMeal - firstMeal <= 1 ) return;
+        setMeals({...meals, firstMeal: firstMeal + 1})
+    }
+
+    const handleLastMealMinusClick = () => {
+        if (lastMeal - firstMeal <= 1 ) return;
+        setMeals({...meals, lastMeal: lastMeal - 1})
+    }
+
+    const handleLastMealPlusClick = () => {
+        if (lastMeal >= 23 ) return;
+        setMeals({...meals, lastMeal: lastMeal + 1})
+    }
+
+    const handleMealsNumberMinusClick = () => {
+        if (mealsNumber <= 2 ) return;
+        setMeals({...meals, mealsNumber: mealsNumber - 1})
+    }
+
+    const handleMealsNumberPlusClick = () => {
+        setMeals({...meals, mealsNumber: mealsNumber + 1})
+    }
+
     return (
         <SettingsSection>
             <h2>Progress Bar</h2>
             <div className="Settings__section-body Settings__time-grid">
-                <ProgressCell count={8}>First Meal At:</ProgressCell>
-                <ProgressCell count={9}>Last Meal At:</ProgressCell>
-                <ProgressCell count={5}>Daily Meals Number:</ProgressCell>        
+                <ProgressCell count={firstMeal} onPlusClick={handleFirstMealPlusClick} onMinusClick={handeFirstMealMinusClick}>First Meal At:</ProgressCell>
+                <ProgressCell count={lastMeal} onPlusClick={handleLastMealPlusClick} onMinusClick={handleLastMealMinusClick}>Last Meal At:</ProgressCell>
+                <ProgressCell count={mealsNumber} onPlusClick={handleMealsNumberPlusClick} onMinusClick={handleMealsNumberMinusClick}>Daily Meals Number:</ProgressCell>        
             </div>                
         </SettingsSection>
     )
