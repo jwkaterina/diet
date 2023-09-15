e2b893c5974d7f847f14a70dbbe5979f32ce256e
jwkaterina <jwkaterina@gmail.com>
2023-09-15 18:53:37 +0200

    handle half click

--- app/component/food/portion/portion.tsx
+++ app/component/food/portion/portion.tsx
@@ -2,14 +2,32 @@
 
 import React, { FunctionComponent } from 'react';
 import { PortionsContext  } from '../../../context/portions-context';
+import { SettingsContext } from '../../../context/settings-context';
 import { Group } from '../../../context/portions-reducer';
 
 import './portion.css'
 
 export const Portion: FunctionComponent<PortionProps> = (props) => {
     const { dispatch} = React.useContext(PortionsContext);
+    const { settings } = React.useContext(SettingsContext);
+
+    enum CheckProps {
+        half = 0.5,
+        full = 1,
+        none = 0
+    }
+    const [check, setCheck] = React.useState<CheckProps>(CheckProps.none);
+
 
     const handleClick = () => {
+        if(settings.halfPortions) {
+            handleHalfPortionClick();
+        } else {
+            handleFullPortionClick();
+        }
+    };
+
+    const handleFullPortionClick = () => {
         if(props.index <= props.checked) {
             dispatch({
                 type: 'check',
@@ -23,11 +41,40 @@ export const Portion: FunctionComponent<PortionProps> = (props) => {
                 checked: props.index,
               }); 
         }
-    };
+    }
 
-    const checked = props.checked >= props.index;
+    const handleHalfPortionClick = () => {
+        console.log("half portion change");
+        if(props.index <= props.checked / 2) {
+            dispatch({
+                type: 'check',
+                group: props.group,
+                checked: 2 * (props.index - 1),
+              }); 
+        } else if(props.index = props.checked / 2 + 0.5) {
+            dispatch({
+                type: 'check',
+                group: props.group,
+                checked: props.index * 2,
+              }); 
+        } else if(props.index >= props.checked / 2 + 1) {
+            dispatch({
+                type: 'check',
+                group: props.group,
+                checked: props.index * 2 - 1,
+              }); 
+        }
+    }
 
-    return <svg width={props.width} height={props.height} viewBox={`0 0 ${props.width} ${props.height}`} className={checked ? "portion-svg checked" : "portion-svg"} onClick={handleClick}>
+    const calculateClassname = () => {
+        if(props.index <= props.checked) {
+            return "portion-svg checked"
+        } else {
+            return "portion-svg"
+        }
+    }
+
+    return <svg width={props.width} height={props.height} viewBox={`0 0 ${props.width} ${props.height}`} className={calculateClassname()} onClick={handleClick}>
 
         {props.children}
 
