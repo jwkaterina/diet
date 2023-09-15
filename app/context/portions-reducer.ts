f25492b8dedc7fcc66870184cb30719cbcd99626
jwkaterina <jwkaterina@gmail.com>
2023-09-15 10:17:40 +0200

    refactor portions reducer

--- app/context/portions-reducer.ts
+++ app/context/portions-reducer.ts
@@ -12,176 +12,152 @@ export const  PortionsReducer = (portions: any, action: any) => {
         case 'check': {
             switch (action.group) {
                 case Group.CARBS: {
-                    localStorage.setItem('portions', JSON.stringify({...portions, carbs: {
+                    const newPortions = {...portions, carbs: {
                         ...portions.carbs, checked: action.checked
-                    }}));
-                    return {...portions, carbs: {
-                    ...portions.carbs, checked: action.checked
-                    }}
+                    }};
+                    localStorage.setItem('portions', JSON.stringify(newPortions));
+                    return newPortions;
                 };
                 case Group.FATS: {
-                    localStorage.setItem('portions', JSON.stringify({...portions, fats: {
+                    const newPortions = {...portions, fats: {
                         ...portions.fats, checked: action.checked
-                    }}));
-                    return {...portions, fats: {
-                    ...portions.fats, checked: action.checked
                     }};
+                    localStorage.setItem('portions', JSON.stringify(newPortions));
+                    return newPortions;
                 }
                 case Group.FRUIT: {
-                    localStorage.setItem('portions', JSON.stringify({...portions, fruit: {
+                    const newPortions = {...portions, fruit: {
                         ...portions.fruit, checked: action.checked
-                    }}));
-                    return {...portions, fruit: {
-                    ...portions.fruit, checked: action.checked
                     }};
+                    localStorage.setItem('portions', JSON.stringify(newPortions));
+                    return newPortions;
                 }
                 case Group.PROTS: {
-                    localStorage.setItem('portions', JSON.stringify({...portions, prots: {
+                    const newPortions = {...portions, prots: {
                         ...portions.prots, checked: action.checked
-                    }}));
-                    return {...portions, prots: {
-                    ...portions.prots, checked: action.checked
                     }};
+                    localStorage.setItem('portions', JSON.stringify(newPortions));
+                    return newPortions;
                 }
                 case Group.SWEETS: {
-                    localStorage.setItem('portions', JSON.stringify({...portions, sweets: {
+                    const newPortions = {...portions, sweets: {
                         ...portions.sweets, checked: action.checked
-                    }}));
-                    return {...portions, sweets: {
-                    ...portions.sweets, checked: action.checked
                     }};
+                    localStorage.setItem('portions', JSON.stringify(newPortions));
+                    return newPortions;
                 }
                 case Group.VEGGIES: {
-                    localStorage.setItem('portions', JSON.stringify({...portions, veggies: {
+                    const newPortions = {...portions, veggies: {
                         ...portions.veggies, checked: action.checked
-                    }}));
-                    return {...portions, veggies: {
-                    ...portions.veggies, checked: action.checked
                     }};
+                    localStorage.setItem('portions', JSON.stringify(newPortions));
+                    return newPortions;
                 }
             }
         }
         case 'add': {
             switch (action.group) {
                 case Group.CARBS: {
-                    localStorage.setItem('portions', JSON.stringify({...portions, carbs: {
+                    const newPortions = {...portions, carbs: {
                         ...portions.carbs, number: portions.carbs.number + 1
-                    }}));
-                    return {...portions, carbs: {
-                    ...portions.carbs, number: portions.carbs.number + 1
                     }};
+                    localStorage.setItem('portions', JSON.stringify(newPortions));
+                    return newPortions;
                 }
                 case Group.FATS: {
-                    localStorage.setItem('portions', JSON.stringify({...portions, fats: {
+                    const newPortions = {...portions, fats: {
                         ...portions.fats, number: portions.fats.number + 1
-                    }}));
-                    return {...portions, fats: {
-                    ...portions.fats, number: portions.fats.number + 1
                     }};
+                    localStorage.setItem('portions', JSON.stringify(newPortions));
+                    return newPortions;
                 }
                 case Group.FRUIT: {
-                    localStorage.setItem('portions', JSON.stringify({...portions, fruit: {
+                    const newPortions = {...portions, fruit: {
                         ...portions.fruit, number: portions.fruit.number + 1
-                    }}));
-                    return {...portions, fruit: {
-                    ...portions.fruit, number: portions.fruit.number + 1
                     }};
+                    localStorage.setItem('portions', JSON.stringify(newPortions));
+                    return newPortions;
                 }
                 case Group.PROTS: {
-                    localStorage.setItem('portions', JSON.stringify({...portions, prots: {
+                    const newPortions = {...portions, prots: {
                         ...portions.prots, number: portions.prots.number + 1
-                    }}));
-                    return {...portions, prots: {
-                    ...portions.prots, number: portions.prots.number + 1
                     }};
+                    localStorage.setItem('portions', JSON.stringify(newPortions));
+                    return newPortions;
                 }
                 case Group.SWEETS: {
-                    localStorage.setItem('portions', JSON.stringify({...portions, sweets: {
+                    const newPortions = {...portions, sweets: {
                         ...portions.sweets, number: portions.sweets.number + 1
-                    }}));
-                    return {...portions, sweets: {
-                    ...portions.sweets, number: portions.sweets.number + 1
                     }};
+                    localStorage.setItem('portions', JSON.stringify(newPortions));
+                    return newPortions;
                 }
                 case Group.VEGGIES: {
-                    localStorage.setItem('portions', JSON.stringify({...portions, veggies: {
+                    const newPortions = {...portions, veggies: {
                         ...portions.veggies, number: portions.veggies.number + 1
-                    }}));
-                    return {...portions, veggies: {
-                    ...portions.veggies, number: portions.veggies.number + 1
                     }};
+                    localStorage.setItem('portions', JSON.stringify(newPortions));
+                    return newPortions;
                 }
             }
         }
         case 'reduce': {
             switch (action.group) {
                 case Group.CARBS: {
-                    localStorage.setItem('portions', JSON.stringify({...portions, carbs: {
+                    const newPortions = {...portions, carbs: {
                         ...portions.carbs, number: portions.carbs.number - 1
-                    }}));
-                    if(portions.carbs.number > 0) {return {...portions, carbs: {
-                        ...portions.carbs, number: portions.carbs.number - 1
-                    }}} else return portions;
+                    }};
+                    localStorage.setItem('portions', JSON.stringify(newPortions));
+                    return newPortions;
                 }
                 case Group.FATS: {
-                    localStorage.setItem('portions', JSON.stringify({...portions, fats: {
+                    const newPortions = {...portions, fats: {
                         ...portions.fats, number: portions.fats.number - 1
-                    }}));
-                    if(portions.fats.number > 0) {return {...portions, fats: {
-                        ...portions.fats, number: portions.fats.number - 1
-                    }}} else return portions;
+                    }};
+                    localStorage.setItem('portions', JSON.stringify(newPortions));
+                    return newPortions;
                 }
                 case Group.FRUIT: {
-                    localStorage.setItem('portions', JSON.stringify({...portions, fruit: {
+                    const newPortions = {...portions, fruit: {
                         ...portions.fruit, number: portions.fruit.number - 1
-                    }}));
-                    if(portions.fruit.number > 0) {return {...portions, fruit: {
-                        ...portions.fruit, number: portions.fruit.number - 1
-                    }}} else return portions;
+                    }};
+                    localStorage.setItem('portions', JSON.stringify(newPortions));
+                    return newPortions;
                 }
                 case Group.PROTS: {
-                    localStorage.setItem('portions', JSON.stringify({...portions, prots: {
+                    const newPortions = {...portions, prots: {
                         ...portions.prots, number: portions.prots.number - 1
-                    }}));
-                    if(portions.prots.number > 0) {return {...portions, prots: {
-                        ...portions.prots, number: portions.prots.number - 1
-                    }}} else return portions;
+                    }};
+                    localStorage.setItem('portions', JSON.stringify(newPortions));
+                    return newPortions;
                 }
                 case Group.SWEETS: {
-                    localStorage.setItem('portions', JSON.stringify({...portions, sweets: {
+                    const newPortions = {...portions, sweets: {
                         ...portions.sweets, number: portions.sweets.number - 1
-                    }}));
-                    if(portions.sweets.number > 0) {return {...portions, sweets: {
-                        ...portions.sweets, number: portions.sweets.number - 1
-                    }}} else return portions;
+                    }};
+                    localStorage.setItem('portions', JSON.stringify(newPortions));
+                    return newPortions;
                 }
                 case Group.VEGGIES: {
-                    localStorage.setItem('portions', JSON.stringify({...portions, veggies: {
+                    const newPortions = {...portions, veggies: {
                         ...portions.veggies, number: portions.veggies.number - 1
-                    }}));
-                    if(portions.veggies.number > 0) {return {...portions, veggies: {
-                        ...portions.veggies, number: portions.veggies.number - 1
-                    }}} else return portions;
+                    }};
+                    localStorage.setItem('portions', JSON.stringify(newPortions));
+                    return newPortions;
                 }
             }
         }
         case 'clear': {
-            localStorage.setItem('portions', JSON.stringify({...portions, 
+            const newPortions = {...portions,
                 carbs: {...portions.carbs, checked: 0},
                 fats: {...portions.fats, checked: 0},
                 fruit: {...portions.fruit, checked: 0},
                 prots: {...portions.prots, checked: 0},
                 sweets: {...portions.sweets, checked: 0},
                 veggies: {...portions.veggies, checked: 0}, 
-            }));
-            return {...portions, 
-                carbs: {...portions.carbs, checked: 0},
-                fats: {...portions.fats, checked: 0},
-                fruit: {...portions.fruit, checked: 0},
-                prots: {...portions.prots, checked: 0},
-                sweets: {...portions.sweets, checked: 0},
-                veggies: {...portions.veggies, checked: 0}, 
-            };          
+            };  
+            localStorage.setItem('portions', JSON.stringify(newPortions));
+            return newPortions;
         }
         default: {
             throw Error('Unknown action: ' + action.type);
