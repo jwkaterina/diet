1dad2a5087f0f8b51416a111608da5855bcbdbae
jwkaterina <jwkaterina@gmail.com>
2023-09-17 12:49:46 +0200

    clean code

---
+++ app/layout/nav/modal/modal.tsx
@@ -0,0 +1,37 @@
+import './modal.css'
+
+import { useContext } from 'react';
+import { PortionsContext } from '@/app/context/portions-context';
+
+export default function Modal(props: ModalProps) {
+    const { dispatch} = useContext(PortionsContext);
+
+    const clearPortions = () => {
+        dispatch({
+            type: 'clear',
+        });
+        props.openState.setModalOpen(false);
+    }
+
+    if (!props.openState.modalOpen) return null;
+
+    return (
+        <div className="ConfirmationComponent">
+            <div className="ConfirmationComponent__window">
+                <h3 className="ConfirmationComponent__title">Are You Sure?</h3>
+                <div className="ConfirmationComponent__controls">
+                    <div className="btn" onClick={() => props.openState.setModalOpen(false)}>Cancel</div>
+                    <div className="btn" onClick={clearPortions}>Confirm</div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export interface ModalProps {
+    openState: {
+        modalOpen: boolean,
+        setModalOpen: (modalOpen: boolean) => void
+    }
+}
+
