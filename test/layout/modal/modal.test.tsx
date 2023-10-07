ce907391ad3db8f0de5aeceaf3b27abcd1ac7859
jwkaterina <jwkaterina@gmail.com>
2023-10-07 11:55:17 +0200

    test modal

---
+++ test/layout/modal/modal.test.tsx
@@ -0,0 +1,34 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import user from '@testing-library/user-event';
+import Modal from '../../../app/layout/modal/modal';
+
+describe('render portion group', function () {
+    function renderComponent() {
+        const openState = {
+            modalOpen: true,
+            setModalOpen: jest.fn(),
+        }
+        
+        render(<Modal openState={openState}/>);
+    }
+
+    it('should render heading and 2 buttons', () => {
+        renderComponent();
+
+        const headings = screen.getAllByRole('heading');
+        screen.logTestingPlaygroundURL();
+
+        const heading = screen.getByRole('heading', {
+            name: /are you sure\?/i
+        });
+        const cancelButton = screen.getByText(/cancel/i);
+        const confirmButton = screen.getByText(/confirm/i);
+
+        expect(heading).toBeInTheDocument();
+        expect(cancelButton).toBeInTheDocument();
+        expect(confirmButton).toBeInTheDocument();
+    });
+})
+
+
