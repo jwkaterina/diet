58709caf9f8bc7a44d1e4ee8a36b70827fa66492
jwkaterina <jwkaterina@gmail.com>
2023-10-08 15:21:22 +0200

    style cells in settings sections

--- app/settings/component/cells/portions-cell.tsx
+++ app/settings/component/cells/portions-cell.tsx
@@ -6,11 +6,11 @@ import CellProps from './cell-props'
 export default function PortionsCell({ children, count, onPlusClick, onMinusClick }: CellProps): JSX.Element {
     
     return (
-        <>
+        <div className="portions-cell">
             <h3 className="Settings__portions-cell">{children}</h3>
             <div className="Settings__number-minus-btn Settings__portions-cell Settings__controls-cell" onClick={onMinusClick}></div>
             <h3 className="Settings__portions-cell Settings__count-cell">{count}</h3>
             <div className="Settings__number-plus-btn Settings__portions-cell Settings__controls-cell" onClick={onPlusClick}></div>
-        </> 
+        </div> 
         )
 }
--- app/settings/component/cells/progress-cell.tsx
+++ app/settings/component/cells/progress-cell.tsx
@@ -5,11 +5,11 @@ import CellProps from './cell-props'
 export default function ProgressCell({ children, count, onPlusClick, onMinusClick }: CellProps): JSX.Element {
     
     return (
-        <>
+        <div className="progress-cell">
             <h3 className="Settings__time-cell">{children}</h3>
             <div className="Settings__number-minus-btn Settings__time-cell Settings__controls-cell" onClick={onMinusClick}></div>
             <h3 className="Settings__time-cell Settings__count-cell">{count}</h3>
             <div className="Settings__number-plus-btn Settings__time-cell Settings__controls-cell" onClick={onPlusClick}></div>
-        </> 
+        </div> 
         )
 }
