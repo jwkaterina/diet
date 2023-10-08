9f8b5bda3f22ba3b1b3dee96d14b7117166d94f4
jwkaterina <jwkaterina@gmail.com>
2023-10-08 11:22:28 +0200

    test progress cell

---
+++ test/settings/component/cells/progress-cell.test.tsx
@@ -0,0 +1,58 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import user from '@testing-library/user-event';
+import ProgressCell from '../../../../app/settings/component/cells/progress-cell';
+
+describe('render portion group', function () {
+
+    it('should render all elements', () => {
+        const props = {
+            children: [],
+            count: 5, 
+            onPlusClick: jest.fn(), 
+            onMinusClick: jest.fn()
+        }
+        
+        render(<ProgressCell { ...props}/>);
+
+        const headings = screen.getAllByRole('heading');
+
+        expect(headings).toHaveLength(2);
+        expect(headings[1]).toHaveTextContent('5');
+        screen.logTestingPlaygroundURL();
+    });
+
+    it('should call onMenuClick', async () => {
+        const props = {
+            children: [],
+            count: 5, 
+            onPlusClick: jest.fn(), 
+            onMinusClick: jest.fn()
+        }
+        
+        const { container } = render(<ProgressCell { ...props}/>);
+        const minusButton = container.querySelector('.Settings__number-minus-btn');
+
+        await user.click(minusButton!);
+
+        expect(props.onMinusClick).toHaveBeenCalled();
+    });
+
+    it('should call onClearClick', async () => {
+        const props = {
+            children: [],
+            count: 5, 
+            onPlusClick: jest.fn(), 
+            onMinusClick: jest.fn()
+        }
+        
+        const { container } = render(<ProgressCell { ...props}/>);
+        const plusButton = container.querySelector('.Settings__number-plus-btn');
+
+        await user.click(plusButton!);
+
+        expect(props.onPlusClick).toHaveBeenCalled();
+    });
+})
+
+
