c0bb0c819dcfefbf2e18b18081909fad6bde2774
jwkaterina <jwkaterina@gmail.com>
2023-10-08 17:22:36 +0200

    test switch section

--- app/settings/component/section/switch-section.tsx
+++ app/settings/component/section/switch-section.tsx
@@ -25,12 +25,12 @@ export default function SwitchSection(): JSX.Element {
             <div className="Settings__section-body Settings__general-grid">
                 <h3 className="Settings__units-cell">Use Half Portions:</h3>
                 <label className="Settings__switch Settings__controls-cell">
-                    <input type="checkbox" onChange={handleHalfPortionsChange} checked={settings.halfPortions}></input>
+                    <input data-testid="portions" type="checkbox" onChange={handleHalfPortionsChange} checked={settings.halfPortions}></input>
                     <span className="Settings__switch-slider Settings__switch-round"></span>
                 </label>   
                     <h3 className="Settings__units-cell">Automatic Portions Reset:</h3>
                 <label className="Settings__switch Settings__controls-cell">
-                    <input type="checkbox" onChange={handleAutoResetChange} checked={settings.autoReset}></input>
+                    <input data-testid="reset" type="checkbox" onChange={handleAutoResetChange} checked={settings.autoReset}></input>
                     <span className="Settings__switch-slider Settings__switch-round"></span>
                 </label>   
             </div>       
---
+++ test/settings/component/section/switch-section.test.tsx
@@ -0,0 +1,48 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import user from '@testing-library/user-event';
+import SwitchSection from '../../../../app/settings/component/section/switch-section';
+import { useSettingsDispatch } from '../../../../app/context/settings-context';
+
+jest.mock('../../../../app/context/settings-context', () => {
+    const originalModule = jest.requireActual('../../../../app/context/settings-context');
+  
+    return {
+      __esModule: true,
+      ...originalModule,
+      useSettingsDispatch: jest.fn(),
+    };
+});
+
+describe('render portions section', function () {
+    const mockedUseDispatch = useSettingsDispatch as jest.Mock;
+    const mockedDispatch = jest.fn();
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+        mockedUseDispatch.mockReturnValue(mockedDispatch);
+        render(<SwitchSection />);
+    });
+
+    it('should dispatch halfPortions' , async () => {
+
+        const portionsCheckbox = screen.getByTestId('portions');
+
+        await user.click(portionsCheckbox);
+
+        expect(mockedDispatch).toBeCalledWith({
+            type: 'setHalfPortions',
+        });
+    });
+
+    it('should dispatch autoReset' , async () => {
+
+        const resetCheckbox = screen.getByTestId('reset');
+
+        await user.click(resetCheckbox);
+
+        expect(mockedDispatch).toBeCalledWith({
+            type: 'setAutoReset',
+        });
+    });
+    });
